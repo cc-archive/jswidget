4629917c9042358292348bae4d80f72094bd6425
paulproteus <paulproteus@committer.creativecommons.org>
2007-07-16 20:20:21 +0000

    work on jswidget begins

---
+++ Makefile
@@ -0,0 +1,2 @@
+source.js: gen_source_js.py template.html
+	python gen_source_js.py
---
+++ gen_source_js.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# This takes template.html and makes a document.write() for it.
+# Later, it could take template.html and make DOM objects instead.
+
+def escape_single_quote(s):
+	return s.replace("'", "\\'")
+
+inp = open('template.html')
+out = open('source.js', 'w')
+for line in inp:
+	escaped_line = escape_single_quote(line.strip())
+	print >> out, "document.write('%s');" % escaped_line
+
