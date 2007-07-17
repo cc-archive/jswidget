b510cbca9346646a24e9f393195d79dc8d4c070e
paulproteus <paulproteus@committer.creativecommons.org>
2007-07-17 00:27:22 +0000

    stub starting point

---
+++ complete.js
@@ -0,0 +1,6 @@
+/* Load the prerequisite JS files */
+
+/* Insert the CSS stylesheet */
+
+/* Insert the template */
+document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/template.js" />');
