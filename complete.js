ed0a12de07f1d87188c9e6f32a5f64b143a5e780
paulproteus <paulproteus@committer.creativecommons.org>
2007-07-17 16:11:43 +0000

    Make complete.js store all JS references

--- complete.js
+++ complete.js
@@ -1,4 +1,10 @@
 /* Load the prerequisite JS files */
+document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/prototype.js" />');
+document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/cc-tooltip.js" />');
+document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/cc-jurisdiction.js" />');
+document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/cc-license.js" />');
+document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/cc-lib-freedoms.js" />');
+document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/init.js" />');
 
 /* Insert the CSS stylesheet */
 
