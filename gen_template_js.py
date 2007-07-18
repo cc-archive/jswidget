11a5e2dd4acc775efda0a6d4a902d7d51ff7a9e0
paulproteus <paulproteus@committer.creativecommons.org>
2007-07-18 20:34:09 +0000

    widget now has jurisdictions from license_xsl/licenses.xml

--- gen_template_js.py
+++ gen_template_js.py
@@ -4,25 +4,56 @@
 
 from simpletal import simpleTAL, simpleTALES
 import cStringIO as StringIO
+import BeautifulSoup
 
-# Create the context that is used by the template
-context = simpleTALES.Context()
-jurisdictions = [{"id": "kr", "name": "Korea"}
-            ]
-context.addGlobal("jurisdictions", jurisdictions)
+LANGUAGE="en_US"
+
+import sys
+sys.path.insert(0, './license_xsl/licensexsl_tools')
+import translate
+
+def grab_license_ids():
+	licenses_xml = BeautifulSoup.BeautifulSoup(open('license_xsl/licenses.xml'))
+	juris = []
+	for juri in licenses_xml('jurisdiction-info'):
+		juris.append(juri['id'])
+	juris = [juri for juri in juris if juri != '-']
+	return juris
+
+def get_PoFile():
+	return translate.PoFile("license_xsl/i18n/i18n_po/icommons-%s.po" % LANGUAGE)
+
+def country_id2name(country_id):
+	# Now gotta look it up with gettext...
+	po = get_PoFile()
+	try:
+		return po['country.%s' % country_id]
+	except KeyError:
+		return country_id
 
 def escape_single_quote(s):
 	return s.replace("'", "\\'")
 
-templateFile = open('template.html')
-template = simpleTAL.compileHTMLTemplate(templateFile)
-templateFile.close()
+def main():
+	# Create the context that is used by the template
+	context = simpleTALES.Context()
+	
+	jurisdiction_names = grab_license_ids()
+	jurisdictions = [ dict(id=juri, name=country_id2name(juri)) for juri in jurisdiction_names]
+	context.addGlobal("jurisdictions", jurisdictions)
 
-output_buffer = StringIO.StringIO()
-template.expand(context, output_buffer)
-out = open('template.js', 'w')
+	templateFile = open('template.html')
+	template = simpleTAL.compileHTMLTemplate(templateFile)
+	templateFile.close()
+	
+	output_buffer = StringIO.StringIO()
+	template.expand(context, output_buffer)
+	out = open('template.js', 'w')
 
-for line in output_buffer.getvalue().split('\n'):
-	escaped_line = escape_single_quote(line.strip())
-	print >> out, "document.write('%s');" % escaped_line
+	for line in output_buffer.getvalue().split('\n'):
+		escaped_line = escape_single_quote(line.strip())
+		print >> out, "document.write('%s');" % escaped_line
 
+
+if __name__ == '__main__':
+	main()
