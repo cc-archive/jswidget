b2578d8c03062d24c6053537b1bf01d792f582b1
paulproteus <paulproteus@committer.creativecommons.org>
2007-07-19 16:46:15 +0000

    dynamic JavaScript generated by PHP to power remote dynamic web applications

--- complete.js
+++ complete.js
@@ -1,3 +1,13 @@
+<?php
+/**
+ * NOTE: Don't be fooled by the extension.  This gets intepreted by the PHP
+ * interpreter.  That way I can dispatch based on the query string.
+ *
+ * See .htaccess for how to configure it for php5.
+ */
+header("content-type: application/x-javascript");
+?>
+
 /* Load the prerequisite JS files */
 document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/prototype.js" />');
 document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/cc-tooltip.js" />');
@@ -6,7 +16,22 @@ document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/c
 document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/cc-lib-freedoms.js" />');
 document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/js/init.js" />');
 
-/* Insert the CSS stylesheet */
+/* NOTE: I do not include the CSS stylesheet
+   and instead I let others style our boxes the way they want. */
 
 /* Insert the template */
-document.write('<script src="http://labs.creativecommons.org/jswidget/trunk/template.js" />');
+
+<?php
+/* First check if we were called with ?locale=XX and dispatch accordingly */
+    if (array_key_exists('locale', $_GET)) {
+	$template_url = 'http://labs.creativecommons.org/jswidget/trunk/template.' . $_GET['locale'] . '.js';
+    }
+
+/* Else, do the generic one and hope they either like English or will
+ * do their own language negotiation */
+    else {
+	$template_url = 'http://labs.creativecommons.org/jswidget/trunk/template.js';
+    }
+?>
+    document.write('<?php echo $template_url; ?>');
+
