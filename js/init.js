e868a6a455d7474cb50facdf5a68a661c970fc9d
pyrak <pyrak@committer.creativecommons.org>
2009-07-02 20:04:59 +0000

    cc0 branch: js now injects stylesheet into html doc

--- js/init.js
+++ js/init.js
@@ -1,3 +1,27 @@
+var stylesheetURL = 'example-widget-style.css'
+
+//thanks, http://www.ibm.com/developerworks/xml/library/x-matters41.html
+function prependChild(parent, node) {
+    parent.insertBefore(node, parent.firstChild);
+}
+
+//insert a stylesheet before all other stylesheets
+//more precisely, it inserts the stylesheet at the beginning of head
+//this way, it is overriden by other stylesheets
+function insert_stylesheet_first(stylesheetURL){
+
+    //build our stylesheet link node
+    var cssNode = document.createElement('link');
+    cssNode.type = 'text/css';
+    cssNode.rel = 'stylesheet';
+    cssNode.href = stylesheetURL;
+    cssNode.media = 'screen';
+    cssNode.title = 'dynamicLoadedSheet';
+    
+    //shove it in there!
+    prependChild(document.getElementsByTagName("head")[0], cssNode);
+}
+
 function cc_js_pageInit() {
     if (cc_js_$('want_cc_license_nah') && cc_js_$('want_cc_license_nah').checked) {
         // then do not init
@@ -6,6 +30,9 @@ function cc_js_pageInit() {
         cc_js_init();
     }
     cc_js_init_tip();
+
+    //shove our stylesheet in before all the others
+    insert_stylesheet_first(stylesheetURL);
 }
 
 if (window.onload) {
